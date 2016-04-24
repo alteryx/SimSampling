f9660dc30958d408c9e2fbc2b900028b40ab013b
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-04-24 12:53:35 -0700

    add interface elements for binned data and roulette.

--- Supporting_Macros/SimSampling1.R
+++ Supporting_Macros/SimSampling1.R
@@ -1,23 +1,20 @@
 ## DO NOT MODIFY: Auto Inserted by AlteryxRhelper ----
 library(AlteryxRhelper)
-## Configuration ----
 config <- list(
+  binnedDataName = textInput('%Question.binnedDataName%'),
+  binnedIdField = dropdownInput('%Question.binnedIdField%'),
+  binnedValueField = dropdownInput('%Question.binnedValueField%'),
   chunkSize = numericInput('%Question.chunkSize%' , 256000),
+  dataKind = dropdownInput('%Question.dataKind%' , 'raw'),
   distribution = dropdownInput('%Question.distribution%' , 'norm'),
   distToFit = listInput('%Question.distToFit%'),
   fields = listInput('%Question.fields%'),
-  jsonBounds = textInput(
-    '%Question.jsonBounds%', 
-    jsonlite::toJSON(list(mean = 0, sd = 1), auto_unbox = TRUE)
-  ),
-  jsonParameters = textInput(
-    '%Question.jsonParameters%',
-    jsonlite::toJSON(c(-Inf, Inf), auto_unbox = TRUE)
-  ),
+  jsonBounds = textInput('%Question.jsonBounds%' , '[-4, 4]'),
+  jsonParameters = textInput('%Question.jsonParameters%' , '{"mean": 0, "sd": 1}'),
   numIterations = numericInput('%Question.numIterations%' , 10),
   samplingMechanism = dropdownInput('%Question.samplingMechanism%' , 'mc'),
   samplingMode = dropdownInput('%Question.samplingMode%' , 'parametric'),
-  samplingStrategy = dropdownInput('%Question.samplingStrategy%'),
+  samplingStrategy = dropdownInput('%Question.samplingStrategy%' , 'rows'),
   seed = numericInput('%Question.seed%' , 1),
   stageName = textInput('%Question.stageName%')
 )
@@ -30,25 +27,9 @@ config$parameters = jsonlite::fromJSON(config$jsonParameters)
 
 print(config)
 
-## Inputs ----
-readInputs <- function(...){
-  inputNames = c(...)
-  streams = paste0('#', seq_along(inputNames))
-  inputs <- setNames(lapply(streams, read.Alteryx), inputNames)
-  Filter(function(d){NROW(d) > 0}, inputs)
-}
-# TOFIX: think through the condition to read inputs
-inputs <- if (inAlteryx() && '%Question.activePage%' != "") {
-  readInputs("S", "D") 
-} else {
-  # replace NULL with default
-  NULL
-}
 
-payload <- list(config = config, inputs = inputs)
-
-## Insert Processing Code ----
-
+# Read Data
+d <- read.Alteryx2("#1", default = data.frame(x = 1:10))
 
 # Write Data to Output 1
-write.Alteryx2(inputs, 1)
+write.Alteryx2(d, 1) 
