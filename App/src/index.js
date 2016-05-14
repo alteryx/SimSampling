115a10ccda638b4cb0782b74171bde477fba0af0
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-05-13 19:27:56 -0700

    update index.js
    This allows the same index.js file to be used in Alteryx and for testing in the browser. It uses the presence of a window.Alteryx.browser variable to detect if the script is running inside Alteryx CEF or not. This can be cleaned up considerably, and I will do that during a refactor.

--- App/src/index.js
+++ App/src/index.js
@@ -18,33 +18,48 @@ var displayControls = [
 
 Alteryx.Gui.BeforeLoad = function BeforeLoad(manager, AlteryxDataItems) {
   const dataItem = makeDataItem(manager, AlteryxDataItems);
-  //dataItem('distribution', { value: DistData.distribution });
-  //dataItem('distributions', { value: JSON.stringify(DistData.distributions) });
-  dataItem('displaySeed', {value: true}, 'SimpleBool')
-  dataItem('intermediate', {value: false}, 'SimpleBool')
+  dataItem('_distribution', { value: DistData.distribution });
+  dataItem('_distributions', { value: JSON.stringify(DistData.distributions) });
   dataItem('rouletteData', { value: JSON.stringify(RouletteData) });
-  dataItem('jsonRouletteData', { value: '{"20": 5, "30": 6}' });
+  dataItem('jsonRouletteData', { value: '{"20": 8, "30": 6}' });
+  if (!window.Alteryx.browser){
+    dataItem('displaySeed', {value: true}, 'SimpleBool')
+    dataItem('intermediate', {value: false}, 'SimpleBool')
+  }
 };
 
 
 Alteryx.Gui.AfterLoad = function AfterLoad(manager) {
   console.log('After Load ----');
   const collection = [
-    { key: 'distributions', type: 'json' },
-    { key: 'distribution', type: 'value' },
+    { key: '_distributions', type: 'json' },
+    { key: '_distribution', type: 'value' },
   ];
   const collection2 = [
     { key: 'rouletteData', type: 'json' },
     { key: 'jsonRouletteData', type: 'json' },
   ];
   //renderComboNumericSliders(manager, collection, 'combonumericslider');
-  controlDisplayIntermediate(manager)
-  controlDisplaySeed(manager)
-  displayControls.forEach(function(d){displayTarget.apply(null, d)})
-  renderRoulette(manager, collection2, 'rouletteChart');
-  Alteryx.Gui.manager.GetDataItemByDataName("dataKind").BindUserDataChanged(function(d){
-    if (d === 'manual') window.dispatchEvent(new Event('resize'));
-  })
+  if (window.Alteryx.browser) {
+    renderRoulette(manager, collection2, 'rouletteChart');
+    renderComboNumericSliders(manager, collection, 'combonumericslider');
+  } else {
+    controlDisplayIntermediate(manager)
+    controlDisplaySeed(manager)
+    displayControls.forEach(function(d){displayTarget.apply(null, d)})
+    renderRoulette(manager, collection2, 'rouletteChart');
+    renderComboNumericSliders(manager, collection, 'combonumericslider');
+    Alteryx.Gui.manager
+      .GetDataItemByDataName("dataKind")
+      .BindUserDataChanged(function(d){
+        if (d === 'manual') window.dispatchEvent(new Event('resize'));
+      })
+    Alteryx.Gui.manager
+      .GetDataItemByDataName("samplingMode")
+      .BindUserDataChanged(function(d){
+         window.dispatchEvent(new Event('resize'));
+      })
+  }
 };
 
 if (window.Alteryx.browser) {
