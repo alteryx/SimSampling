ac729bf116d621ce447a47748d321ac0a9b2cb82
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-05-25 16:57:35 -0700

    add initial logic to handle discrete distributions.

--- App/src/stores/DistStore.js
+++ App/src/stores/DistStore.js
@@ -3,13 +3,21 @@ import { jStat } from 'jstat';
 import { computed, toJSON } from 'mobx';
 
 const DIST_DATA_FN = dist => p => {
-  const nCount = 1000
-  const xmin = jStat[dist].inv.apply(null, [0.001].concat(p))
-  const xmax = jStat[dist].inv.apply(null, [0.999].concat(p))
-  return d3.range(xmin, xmax, (xmax - xmin)/nCount).map(d => (
-    { x: d, y: jStat[dist].pdf.apply(null, [d].concat(p)) }
-  ))
-  
+  if (dist != "binomial"){
+    const nCount = 1000
+    const xmin = jStat[dist].inv.apply(null, [0.001].concat(p))
+    const xmax = jStat[dist].inv.apply(null, [0.999].concat(p))
+    return d3.range(xmin, xmax, (xmax - xmin)/nCount).map(d => (
+      { x: d, y: jStat[dist].pdf.apply(null, [d].concat(p)) }
+    ))
+  } else {
+    const xmin = 0;
+    const xmax = p[0];
+    const nCount = p[0]
+    return d3.range(xmin, xmax, (xmax - xmin)/nCount).map(d => (
+      { x: d, y: jStat[dist].pdf.apply(null, [d].concat(p)) }
+    ))
+  }
 }
 
 class DistStore extends ObservableStore {
