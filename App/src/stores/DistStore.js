bc8fdb4d7e4f6023e603bff2cda44827c13d0507
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-05-26 15:07:58 -0700

    add logic to support discrete distributions.
    this can be refactored to accommodate a larger class of discrete distributions.

--- App/src/stores/DistStore.js
+++ App/src/stores/DistStore.js
@@ -2,8 +2,33 @@ import ObservableStore from './ObservableStore';
 import { jStat } from 'jstat';
 import { computed, toJSON } from 'mobx';
 
+const distDataFnDiscrete = dist => p => {
+  const distObj = jStat[dist];
+  const nCount = 1000;
+
+  let xmin = 0;
+  while(distObj.cdf(xmin, p) < 0.001) {
+    xmin++;
+  }
+
+  let xmax = 0;
+  while(distObj.cdf(xmax, p) <= 0.999) {
+    xmax++;
+  }
+
+  return d3.range(xmin, xmax, (xmax - xmin)/nCount).map(d => (
+//    { x: d, y: distObj.pdf.apply(null, [d].concat(p)) }
+    //{ x: d, y: distObj.pdf(...[d].concat(p)) }
+    { x: d, y: distObj.pdf(d, p) }
+  ));
+};
+
+function isDiscrete(dist){
+  return ['binomial', 'poisson', 'geometric'].indexOf(dist) >= 0
+}
+
 const DIST_DATA_FN = dist => p => {
-  if (dist != "binomial"){
+  if (!isDiscrete(dist)){
     const nCount = 1000
     const xmin = jStat[dist].inv.apply(null, [0.001].concat(p))
     const xmax = jStat[dist].inv.apply(null, [0.999].concat(p))
@@ -11,9 +36,14 @@ const DIST_DATA_FN = dist => p => {
       { x: d, y: jStat[dist].pdf.apply(null, [d].concat(p)) }
     ))
   } else {
+    if (dist == 'geometric'){
+      dist = 'negbin'
+      p = [1, p[0]]
+      console.log(p)
+    }
     const xmin = 0;
-    const xmax = p[0];
-    const nCount = p[0]
+    const xmax = dist == 'binomial' ? p[0] : dist == 'negbin' ? 20: p[0] + 3*Math.sqrt(p[0]);
+    const nCount = xmax
     return d3.range(xmin, xmax, (xmax - xmin)/nCount).map(d => (
       { x: d, y: jStat[dist].pdf.apply(null, [d].concat(p)) }
     ))
