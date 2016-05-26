ed63a5d25de360cf7c1b2b3736517647c78b1e4b
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-05-25 18:32:45 -0700

    remove stray DOM element from D3Component.

--- App/src/components/D3Component.js
+++ App/src/components/D3Component.js
@@ -1,22 +1,20 @@
 import { observer } from 'mobx-react';
 import React, { Component } from 'react';
+import d3 from 'd3';
 
 @observer
 class D3Component extends React.Component {
   componentDidMount(){
-    const el = this.refs.areachart
-    this.chart = new this.props.chart(el).data(this.data)
+    const el = this.refs[this.props.chartType];
+    this.chart = new this.props.chart(el).data(this.data);
   }
   componentDidUpdate(){
     this.chart.data(this.data)
-  }   
+  }
   render(){
     this.data = this.props.store.data
     return (
-      <div> 
-        <b>{this.props.store.dist}</b>
-        <div ref='areachart'></div>
-      </div>
+      <div ref={this.props.chartType}></div>
     )
   }
 }
