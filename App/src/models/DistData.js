99c30f2932a988f0c8f359c27086e6d3572c2388
Dylan Blanchard <dylblanchard@gmail.com>
2016-05-25 19:45:19 -0500

    merge distdata changes

--- App/src/models/DistData.js
+++ App/src/models/DistData.js
@@ -8,22 +8,6 @@ const DistData = {
         discrete: false
     */
     normal: [
-<<<<<<< HEAD
-      {label: "Mean", paramName: "mean", value: 0, min: -5, max: 5, step: 1},
-      {label: "Stdev", paramName: "sd", value: 1, min: 0, max: 2, step: 0.1, minSpinner: 0}
-      ],
-    gamma: [
-      {label: "shape", paramName: "shape", value: 1, min: 0.1, max: 5, step: 0.1, minSpinner: 0.0000001},
-      {label: "scale", paramName: "scale", value: 1, min: 0.1, max: 5, step: 0.1, minSpinner: 0.0000001}
-      ],
-    lognormal: [
-      {label: "Mean", paramName: "meanlog", value: 0, min: -5, max: 5, step: 1},
-      {label: "Stdev", paramName: "sdlog", value: 1, min: 0, max: 2, step: 0.1, minSpinner: 0}
-      ],
-    pareto: [
-      {label: "scale", paramName: "scale", value: 2, min: 0.1, max: 5, step: 0.1, minSpinner: 0.0000001},
-      {label: "shape", paramName: "shape", value: 2, min: 0.1, max: 5, step: 0.1, minSpinner: 0.0000001}
-=======
       {label: "Mean", paramName: "mean", value: 0, min: -5, max: 5, step: 1e-2},
       {label: "Stdev", paramName: "sd", value: 1, min: 0, max: 2, step: 1e-2}
       ],
@@ -38,7 +22,6 @@ const DistData = {
     pareto: [
       {label: "scale", paramName: "scale", value: 2, min: 0.1, max: 5, step: 1e-2},
       {label: "shape", paramName: "shape", value: 2, min: 0.1, max: 5, step: 1e-2}
->>>>>>> 95ef345dc029e8709132d61c44ae8313caa931d2
       ],  
     uniform: [
       {label: "min", paramName: "min", value: -1, min: -10, max: 10, step: 1e-2},
@@ -50,19 +33,11 @@ const DistData = {
       {label: "most likely", paramName: "c", value: 0, min: -10, max: 10, step: 1e-2}
     ],
     poisson: [
-<<<<<<< HEAD
-      {label: "mean", paramName: "lambda", value: 5, min: 0.1, max: 5, step: 0.1, minSpinner: 0.0000001}
-    ],
-    binomial: [
-      {label: "trials", paramName: "size", value: 10, min: 1, max: 20, step: 1, minSpinner: 0, discrete: true},
-      {label: "probability", paramName: "prob", value: 0.5, min: 0, max: 1, step: 0.1, minSpinner: 0, maxSpinner: 1}
-=======
       {label: "mean", paramName: "lambda", value: 5, min: 0.1, max: 5, step: 1e-2}
     ],
     binomial: [
       {label: "trials", paramName: "size", value: 10, min: 1, max: 20, step: 1e-2},
       {label: "probability", paramName: "prob", value: 0.5, min: 0, max: 1, step: 1e-2}
->>>>>>> 95ef345dc029e8709132d61c44ae8313caa931d2
     ]
   },
   distribution: 'normal',
